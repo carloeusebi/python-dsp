1829dfb53dcdebab0d9a1b46f7fc08cf7c89fb77
carloeusebi <carloeusebi@gmail.it>
2023-07-24 23:01:01 +0200

    Added cli interface to select survey from api response

--- src/dsp/dsp.py
+++ src/dsp/dsp.py
@@ -1,9 +1,13 @@
 from InquirerPy import prompt
 
 from website.website import Website
-from utils import utils
+from utils import utils, choose_survey
 
 API_URL = "https://www.carloeusebiwebdeveloper.it/api"
+PATIENTS_ENDPOINT = "patients"
+SURVEYS_ENDPOINT = "surveys"
+PATIENTS_ORDER = "lname"
+SURVEYS_ORDER = "last_updated"
 
 
 def get_credentials():
@@ -15,12 +19,35 @@ def get_credentials():
     )
 
 
+def map_surveys(surveys: list[dict], patients: list[dict]) -> list[dict]:
+    completed_surveys: list[dict] = []
+    for survey in surveys:
+        patient_id = survey["patient_id"]
+        for patient in patients:
+            if patient["id"] == patient_id:
+                survey["patient_name"] = f"{patient['fname']} {patient['lname']}"
+        if survey["completed"]:
+            completed_surveys.append(survey)
+    return completed_surveys
+
+
 def main():
     utils.clearscreen()
     website = Website(API_URL)
+    # login
     while not website.login(get_credentials()):
         pass
     print("Accesso riuscito")
+    # get surveys and patients from website's api
+    print("Download in corso... [-------------------------]", end="\r")
+    row_surveys = website.get(SURVEYS_ENDPOINT, SURVEYS_ORDER)
+    print("Download in corso... [=============------------]", end="\r")
+    patients = website.get(PATIENTS_ENDPOINT, PATIENTS_ORDER)
+    utils.clearscreen()
+    # filters completed surveys and add patient's name
+    surveys = map_surveys(row_surveys, patients)
+    selected_survey = choose_survey.choose_survey(surveys, patients)
+    print(selected_survey)
 
 
 if __name__ == "__main__":
